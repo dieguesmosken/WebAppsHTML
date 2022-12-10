6cd8bdb4fb224702650f4efcbe5b42ec972f4eab
TeuzDev <matheus.diegues@outlook.com>
2022-12-10 13:34:05 -0300

    Calculator v0.1 launched

--- IOS_Calculator_html/index.js
+++ IOS_Calculator_html/index.js
@@ -1,11 +1,11 @@
 const numbers = document.querySelectorAll('.numbers');
 const result = document.querySelector('.result span');
-const signs = document.querySelectorAll('.signs');
+const signs = document.querySelectorAll('.sign');
 const clear = document.querySelector('.clear');
 const equal = document.querySelector('.equal');
 const negative = document.querySelector('.negative');
 const percent = document.querySelector('.percent');
-
+console.log("carregamento concluido");
 
 
 let firstValue = "";
@@ -14,62 +14,81 @@ let secondValue = "";
 let isSecondValue = false;
 let sign = "";
 let resultValue = 0;
+console.log("declaração de variaveis concluida");
 
 for (let i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', (e) => {
         let atr = e.target.getAttribute('value');
+        console.log(atr);
         if (isFirstValue === false) {
-            getFirstValue(atr);
+            getFirstValue(atr)
+            console.log(atr);
         }
         if (isSecondValue === false) {
-            getSecondValue(atr);
+            getSecondValue(atr)
         }
     })
 
 }
+console.log("for concluido");
 function getFirstValue(el) {
     result.innerHTML = "";
     firstValue += el;
     result.innerHTML = firstValue;
     firstValue = +firstValue;
-    isFirstValue = true;
-
+    //isFirstValue = true;
+    console.log("get first value concluido");
 }
 function getSecondValue(el) {
     if (firstValue != "" && sign != "") {
         secondValue += el;
         result.innerHTML = secondValue;
         secondValue = +secondValue;
-        isSecondValue = true;
+        console.log("if concluido");
     }
+    console.log("get second value concluido");
 }
 function getSign() {
     for (let i = 0; i < signs.length; i++) {
         signs[i].addEventListener('click', (e) => {
             sign = e.target.getAttribute('value');
+            console.log(sign);
             isFirstValue = true;
-            isSecondValue = false;
+            //isSecondValue = false;
         })
     }
+    console.log("get sign concluido");
 }
 getSign();
 
 equal.addEventListener('click', () => {
     result.innerHTML = "";
-    if (sign === "+") {
-        resultValue = firstValue + secondValue;
-    } else if (sign === "-") {
-        resultValue = firstValue - secondValue;
-    } else if (sign === "x") {
-        resultValue = firstValue * secondValue;
-    } else if (sign === "/") {
-        resultValue = firstValue / secondValue;
-    }
+   // if (sign != "") {
+        if (sign === "+") {
+            console.log("entrou no if");
+            console.log(firstValue);
+            console.log("apertou o mais");
+            resultValue = firstValue + secondValue;
+        } else if (sign === "-") {
+            console.log("apertou o menos");
+            resultValue = firstValue - secondValue;
+        } else if (sign === "x") {
+            console.log("apertou o vezes");
+            resultValue = firstValue * secondValue;
+        } else if (sign === "/") {
+            console.log("apertou o dividir");
+            resultValue = firstValue / secondValue;
+        }
+    // } else {
+    //     resultValue = firstValue;
+    // }
     result.innerHTML = resultValue;
     firstValue = resultValue;
     secondValue = "";
 
     checkResultLength();
+    console.log("apertou o igual");
+    console.log(resultValue);
 
 })
 
@@ -91,7 +110,7 @@ negative.addEventListener('click', () => {
     if (firstValue === "" && secondValue != "" && sign != "") {
         resultValue = -resultValue;
     }
-
+    console.log("apertou o mais ou menos");
     result.innerHTML = resultValue;
 })
 
@@ -105,6 +124,7 @@ percent.addEventListener('click', () => {
         resultValue = resultValue / 100;
     }
     result.innerHTML = resultValue;
+    console.log("apertou o porcento");
 })
 
 clear.addEventListener('click', () => {
@@ -115,4 +135,5 @@ clear.addEventListener('click', () => {
     resultValue = 0;
     isFirstValue = false;
     isSecondValue = false;
+    console.log("apertou o clear");
 })
