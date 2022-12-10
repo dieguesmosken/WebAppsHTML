6729fdc01469724ae3efc5cf3c2d144db90a084a
TeuzDev <matheus.diegues@outlook.com>
2022-12-10 20:08:51 -0300

    new pacman files 	# new file:   Web_Games/PACMAN-JS/assets/animations.gif 	# new file:   Web_Games/PACMAN-JS/assets/ghost.png 	# new file:   Web_Games/PACMAN-JS/assets/map.png 	# new file:   Web_Games/PACMAN-JS/game.js 	# new file:   Web_Games/PACMAN-JS/ghost.js 	# new file:   Web_Games/PACMAN-JS/index.html 	# new file:   Web_Games/PACMAN-JS/pacman.js

---
+++ Web_Games/PACMAN-JS/game.js
@@ -0,0 +1,185 @@
+const canvas = document.getElementById('canvas');
+const canvasContext = canvas.getContext('2d');
+const pacmanFrames = document.getElementById('animations');
+const ghostFrames = document.getElementById('ghosts');
+
+let createRect = (x, y, width, height, color) => {
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, height);
+};
+
+let fps = 30;
+let oneBlockSize = 20;
+let wallColor = "#342dca";
+let wallSpaceWidth = oneBlockSize / 1.5;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "#000000";
+
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_DOWN = 1;
+// let lives = 3;
+// let ghostCount = 4;
+// let ghostImageLocations = [
+//     { x: 0, y: 0 },
+//     { x: 176, y: 0 },
+//     { x: 0, y: 121 },
+//     { x: 176, y: 121 },
+// ];
+
+let map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2],
+    [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1],
+    [1, 1, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 1, 1],
+    [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+let gameLoop = () => {
+    update();
+    draw();
+}
+
+let update = () => {
+    pacman.moveProcess();
+    pacman.eat();
+    // pacman.update();
+    // ghost.update();
+}
+
+let draw = () => {
+    createRect(0, 0, canvas.width, canvas.height, 'black');
+    //canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    drawWalls();
+    pacman.draw();
+    // ghost.draw();
+}
+
+let gameInterval = setInterval(gameLoop, 1000 / fps);
+
+let drawWalls = () => {
+    for (let i = 0; i < map.length; i++) {
+        for (let j = 0; j < map[0].length; j++) {
+            if (map[i][j] === 1) {
+                createRect(
+                    j * oneBlockSize,
+                    i * oneBlockSize,
+                    oneBlockSize,
+                    oneBlockSize,
+                    wallColor
+                );
+                if(j > 0 && map[i][j-1] === 1) {
+                    createRect(
+                        j * oneBlockSize,
+                        i * oneBlockSize + wallOffset,
+                        wallSpaceWidth + wallOffset,
+                        wallSpaceWidth,
+                        wallInnerColor
+                    );
+                }
+                if(j < map[0].length - 1 && map[i][j+1] === 1) {
+                    createRect(
+                        j * oneBlockSize + wallOffset,
+                        i * oneBlockSize + wallOffset,
+                        wallSpaceWidth + wallOffset,
+                        wallSpaceWidth,
+                        wallInnerColor
+                    );
+                }
+                if(i > 0 && map[i-1][j] === 1) {
+                    createRect(
+                        j * oneBlockSize + wallOffset,
+                        i * oneBlockSize,
+                        wallSpaceWidth,
+                        wallSpaceWidth + wallOffset,
+                        wallInnerColor
+                    );
+                }
+                if(i < map.length - 1 && map[i+1][j] === 1) {
+                    createRect(
+                        j * oneBlockSize + wallOffset,
+                        i * oneBlockSize + wallOffset,
+                        wallSpaceWidth,
+                        wallSpaceWidth + wallOffset,
+                        wallInnerColor
+                    );
+                }
+            }
+        }
+    }
+}
+
+let createNewPacman = () => {
+    pacman = new Pacman(
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize / 5
+    );
+}
+createNewPacman();
+gameLoop();
+
+
+// window.addEventListener('keydown', (e) => {
+//     switch (e.key) {
+//         case 'ArrowUp':
+//             pacman.moveUp();
+//             break;
+//         case 'ArrowDown':
+//             pacman.moveDown();
+//             break;
+//         case 'ArrowLeft':
+//             pacman.moveLeft();
+//             break;
+//         case 'ArrowRight':
+//             pacman.moveRight();
+//             break;
+//     }
+// });
+window.addEventListener('keydown', (event) => {
+    let k = event.keyCode
+
+    setTimeout(() => {
+        if (k === 37 || k === 65)  { // left 
+            pacman.nextDirection = DIRECTION_LEFT;
+        }
+
+        if (k === 38 || k === 87) { // up
+            pacman.nextDirection = DIRECTION_UP;
+        }
+            
+        if (k === 39 || k === 68) { // right
+            pacman.nextDirection = DIRECTION_RIGHT;
+            
+        }
+
+        if (k === 40 || k === 83) { // down
+            pacman.nextDirection = DIRECTION_DOWN;
+            
+        }
+            
+    }, 1);
+
+
+});
---
+++ Web_Games/PACMAN-JS/pacman.js
@@ -0,0 +1,149 @@
+class Pacman {
+    constructor(x, y, width, height, speed) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.direction = DIRECTION_RIGHT;
+        this.nextDirection = this.direction;
+        this.currentFrame = 1;
+        this.frameCount = 7;
+
+        setInterval(() => {
+            this.changeAnimation();
+        }, 100);
+
+
+
+    }
+
+    moveProcess() {
+        this.changeDirectionIfPossible();
+        this.moveForwards();
+        if (this.checkCollision()) {
+            this.moveBackwards();
+        }
+        this.checkGhostCollision();
+    }
+    eat() {
+        this.isEating = true;
+    }
+
+    moveBackwards() {
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y += this.speed;
+                break;
+            case DIRECTION_DOWN:
+                this.y -= this.speed;
+                break;
+        }
+    }
+
+    moveForwards() {
+        switch (this.direction) {
+            case DIRECTION_RIGHT:
+                this.x += this.speed;
+                break;
+            case DIRECTION_LEFT:
+                this.x -= this.speed;
+                break;
+            case DIRECTION_UP:
+                this.y -= this.speed;
+                break;
+            case DIRECTION_DOWN:
+                this.y += this.speed;
+                break;
+        }
+    }
+
+    checkCollision() {
+        let isCollided = false;
+        if (
+            map[this.getMapY()][this.getMapX()] === 1 ||
+            map[this.getMapYRightSide()][this.getMapX()] === 1 ||
+            map[this.getMapY()][this.getMapXRightSide()] === 1 ||
+            map[this.getMapYRightSide()][this.getMapXRightSide()] === 1) {
+            return true;
+        }
+        return false;
+    }
+
+    checkGhostCollision() {
+
+    }
+
+    changeDirectionIfPossible() {
+        switch (this.nextDirection) {
+            case DIRECTION_RIGHT:
+                if (map[this.getMapY()][this.getMapXRightSide()] !== 1) {
+                    this.direction = this.nextDirection;
+                }
+                break;
+            case DIRECTION_LEFT:
+                if (map[this.getMapY()][this.getMapX()] !== 1) {
+                    this.direction = this.nextDirection;
+                }
+                break;
+            case DIRECTION_UP:
+                if (map[this.getMapY()][this.getMapX()] !== 1) {
+                    this.direction = this.nextDirection;
+                }
+                break;
+            case DIRECTION_DOWN:
+                if (map[this.getMapYRightSide()][this.getMapX()] !== 1) {
+                    this.direction = this.nextDirection;
+                }
+                break;
+        }
+    }
+
+    changeAnimation() {
+        this.currentFrame = this.currentFrame === this.frameCount ? 1 : this.currentFrame + 1;
+    }
+
+
+    draw() {
+        canvasContext.save();
+        canvasContext.translate(this.x + oneBlockSize / 2,
+            this.y + oneBlockSize / 2
+        );
+        canvasContext.rotate(this.direction * 90 * Math.PI / 180);
+        canvasContext.translate(
+            -this.x - oneBlockSize / 2,
+            -this.y - oneBlockSize / 2
+        );
+
+        canvasContext.drawImage(
+            pacmanFrames,
+            (this.currentFrame - 1) * oneBlockSize,
+            0,
+            oneBlockSize,
+            oneBlockSize,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+        canvasContext.restore();
+    }
+    getMapX() {
+        return parseInt(this.x / oneBlockSize);
+    }
+    getMapY() {
+        return parseInt(this.y / oneBlockSize);
+    }
+    getMapXRightSide() {
+        return parseInt((this.x + 0.999 * oneBlockSize) / oneBlockSize);
+    }
+    getMapYRightSide() {
+        return parseInt((this.y + 0.999 * oneBlockSize) / oneBlockSize);
+    }
+}
