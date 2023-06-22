3836167cf7428c150b8ec88823a951995e4c83cc
Matheus Mösken Diegues <69434680+dieguesmosken@users.noreply.github.com>
2023-06-22 19:46:48 -0300

    projeto futuro de load

--- Apps/Reprodutor de musica/Reprodutor 3/script.js
+++ Apps/Reprodutor de musica/Reprodutor 3/script.js
@@ -1,89 +1,124 @@
-'use strict'
+const fileInput = document.querySelector('#file-input');
+const playlist = document.querySelector('.playlist');
+const tracks = playlist.querySelectorAll('li');
+const curr_track = document.createElement('audio');
 
-let seek_slider = document.querySelector(".seek_slider");
-let volume_slider = document.querySelector(".volume_slider");
-let curr_time = document.querySelector(".current-time");
-let total_duration = document.querySelector(".total-duration");
+let index = 0;
 
-let isPlaying = false;
-let updateTimer;
-//Variável de controle
-var play = false;
-let curr_track = document.createElement('audio');
-//Cria o componente de áudio especificando a URL
-var audio = new Audio('audio/musica1.mp3');
+// Adiciona um evento de clique para cada botão de reprodução
+tracks.forEach((track, i) => {
+  const playBtn = track.querySelector('.play-btn');
+  const src = track.getAttribute('data-src');
 
-
-function seekTo() {
-    // Calculate the seek position by the
-    // percentage of the seek slider 
-    // and get the relative duration to the track
-    let seekto = curr_track.duration * (seek_slider.value / 100);
-
-    // Set the current track position to the calculated seek position
-    curr_track.currentTime = seekto;
-}
-
-function setVolume() {
-    // Set the volume according to the
-    // percentage of the volume slider set
-    curr_track.volume = volume_slider.value / 100;
-}
-
-function seekUpdate() {
-    let seekPosition = 0;
-
-    // Check if the current track duration is a legible number
-    if (!isNaN(curr_track.duration)) {
-        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
-        seek_slider.value = seekPosition;
-
-        // Calculate the time left and the total duration
-        let currentMinutes = Math.floor(curr_track.currentTime / 60);
-        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
-        let durationMinutes = Math.floor(curr_track.duration / 60);
-        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
-
-        // Add a zero to the single digit time values
-        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
-        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
-        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
-        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
-
-        // Display the updated duration
-        curr_time.textContent = currentMinutes + ":" + currentSeconds;
-        total_duration.textContent = durationMinutes + ":" + durationSeconds;
-    }
-}
-
-document.querySelector('#play').addEventListener('click', () => {
-
-    if (play)
-        return;
-
-    //Reproduz o áudio
-    audio.play();
-
-
-    play = true;
-    updateTimer = setInterval(seekUpdate(), 1000);
+  playBtn.addEventListener('click', () => {
+    index = i;
+    loadTrack(src, track);
+    playTrack();
+  });
 });
 
-document.querySelector('#pause').addEventListener('click', () => {
+// Função para carregar uma faixa na lista de reprodução
+function loadTrack(src, track) {
+  curr_track.src = src;
 
-    //Pausa o áudio
-    audio.pause();
+  // Remove a classe 'active' de todas as faixas da lista de reprodução
+  tracks.forEach(track => {
+    track.classList.remove('active');
+  });
 
-    play = false;
+  // Adiciona a classe 'active' à faixa selecionada
+  track.classList.add('active');
+
+  // Atualiza o título da página com o nome da faixa selecionada
+  document.title = track.querySelector('span').textContent;
+}
+
+// Função para reproduzir uma faixa na lista de reprodução
+function playTrack() {
+  curr_track.play();
+  playpause_btn.innerHTML = '<i class="fas fa-pause-circle"></i>';
+}
+
+// Função para pausar uma faixa na lista de reprodução
+function pauseTrack() {
+  curr_track.pause();
+  playpause_btn.innerHTML = '<i class="fas fa-play-circle"></i>';
+}
+
+// Função para formatar o tempo em segundos para um formato de hora legível
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60);
+  seconds = Math.floor(seconds % 60);
+  return `${minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+}
+
+// Atualiza a barra de progresso da faixa atual
+function updateProgress() {
+  seek_slider.value = curr_track.currentTime;
+  current_time.textContent = formatTime(curr_track.currentTime);
+}
+
+// Atualiza a barra de progresso e o tempo atual e total da faixa atual durante a reprodução
+function updateTrackInfo() {
+  seek_slider.max = curr_track.duration;
+  total_duration.textContent = formatTime(curr_track.duration);
+}
+
+// Avança para a próxima faixa na lista de reprodução
+function nextTrack() {
+  if (index < tracks.length - 1) {
+    index++;
+  } else {
+    index = 0;
+  }
+  const nextTrack = tracks[index];
+  const nextSrc = nextTrack.getAttribute('data-src');
+  loadTrack(nextSrc, nextTrack);
+  playTrack();
+}
+
+// Adiciona um evento de clique ao botão de carregamento de arquivos
+fileInput.addEventListener('change', () => {
+  const files = fileInput.files;
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const src = URL.createObjectURL(file);
+    const filename = file.name;
+    const track = document.createElement('li');
+    track.innerHTML = `<span>${filename}</span><button class="play-btn"><i class="fas fa-play-circle"></i></button>`;
+    track.setAttribute('data-src', src);
+    playlist.appendChild(track);
+    track.querySelector('.play-btn').addEventListener('click', () => {
+      index = i;
+      loadTrack(src, track);
+      playTrack();
+    });
+  }
 });
 
-document.querySelector('#stop').addEventListener('click', () => {
+// Adiciona um evento de clique ao botão de reprodução/pausa
+playpause_btn.addEventListener('click', () => {
+  const isPlaying = curr_track.paused ? false : true;
+  if (isPlaying) {
+    pauseTrack();
+  } else {
+    playTrack();
+  }
+});
 
-    //Pausa o áudio
-    audio.pause();
+// Adiciona um evento de clique ao botão de avançar para a próxima faixa
+next_btn.addEventListener('click', nextTrack);
 
-    //Volta para o ínicio do track
-    audio.currentTime = 0;
+// Atualiza a barra de progresso e o tempo atual e total da faixa atual durante a reprodução
+curr_track.addEventListener('timeupdate', updateProgress);
+curr_track.addEventListener('loadedmetadata', updateTrackInfo);
 
-    play = false;
+// Atualiza a posição da faixa atual quando o usuário arrasta a barra de progresso
+seek_slider.addEventListener('input', () => {
+  curr_track.currentTime = seek_slider.value;
+});
+
+// Atualiza o volume da faixa atual quando o usuário arrasta a barra de volume
+volume_slider.addEventListener('input', () => {
+  curr_track.volume = volume_slider.value;
 });
